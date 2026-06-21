92f850e3a951dcde08760e6ee3d0041a716f8e94
Josue <josue14ortega@gmail.com>
2026-06-20 22:29:44 -0600

    Feat: Completado Laboratorio 9 - Busqueda Binaria

---
+++ Basics/taller13.py
@@ -0,0 +1,30 @@
+"""
+Autor: Josue Ortega
+Fecha: 20-junio-2026
+Taller #13
+Proyecto: Algoritmo de Búsqueda Binaria (Binary Search)
+Descripción: Implementación eficiente de búsqueda logarítmica con rastreo de ruta.
+"""
+
+def binary_search(search_list, value):
+    path_to_target = []
+    low = 0
+    high = len(search_list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        value_at_middle = search_list[mid]
+        path_to_target.append(value_at_middle)
+
+        if value == value_at_middle:
+            return path_to_target, f'Value found at index {mid}'
+        elif value > value_at_middle:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return [], 'Value not found'
+
+print(binary_search([1, 2, 3, 4, 5], 3))
+print(binary_search([1, 2, 3, 4, 5, 9], 4))
+print(binary_search([1, 3, 5, 9, 14, 22], 10))
---
+++ Certificacion/03-polygon-area-calculator/main.py
@@ -0,0 +1,7 @@
+from shape_calculator import Rectangle, Square
+
+rect = Rectangle(10, 5)
+print(rect)
+
+sq = Square(5)
+print(sq)
